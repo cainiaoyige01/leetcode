e5474490a7d6741dfa3edb45cdd1ab1c80032231
cainiaoyige01 <403214324@qq.com>
2023-05-12 16:09:42 +0800

    niuzai

--- string/02_valid_parentheses.go
+++ string/02_valid_parentheses.go
@@ -12,6 +12,38 @@ func main() {
 	fmt.Println(isValid("([)]"))
 }
 
-func isValid(s string) bool {
-	return false
+// isValid
+func isValid(str string) bool {
+	//rune 类似与int32 会自动按照字符独立的单位处理
+	var runes []rune
+	for _, v := range str {
+		switch v {
+		case '{', '(', '[':
+			runes = append(runes, v)
+		case '}', ')', ']':
+			if len(runes) > 0 && opposite(v) == runes[len(runes)-1] {
+				//这是秒点之一 缩小组数 就是弹掉的那一部门
+				runes = runes[0 : len(runes)-1]
+			} else {
+				return false
+			}
+		}
+	}
+	if len(runes) > 0 {
+		return false
+	}
+	return true
+}
+
+// opposite 目的是为了反弹出去 也就是类似与弹栈
+func opposite(v int32) rune {
+	switch v {
+	case '}':
+		return '{'
+	case ')':
+		return '('
+	case ']':
+		return '['
+	}
+	return '\n'
 }
